c05730949ed8cc075fdab8f779258ed510d297cf
NahuelDev23 <nahuel.dev.23@gmail.com>
2022-01-28 13:31:24 -0300

    responsive dashbaord

--- src/components/dashboard/TrList.js
+++ src/components/dashboard/TrList.js
@@ -1,6 +1,6 @@
-import { Button, Td, Tr } from '@chakra-ui/react';
 import React from 'react';
-
+import { Button, IconButton, Menu, MenuButton, MenuList, Td, Tr } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons'
 export const TrList = ({ review, handleEdit, onOpen, setIdToDelete, setIsOpenDelete }) => {
 
     const { title, stars } = review.data()
@@ -10,7 +10,7 @@ export const TrList = ({ review, handleEdit, onOpen, setIdToDelete, setIsOpenDel
             <Tr >
                 <Td textAlign='center'>{title}</Td>
                 <Td textAlign='center'>{stars} / 5</Td>
-                <Td textAlign='center' display='flex'>
+                <Td textAlign='center' display={{ base: 'none', md: 'flex' }}>
                     <Button bgColor='twitter.400' mr='4'
                         _hover={{
                             background: "blue.400",
@@ -27,6 +27,34 @@ export const TrList = ({ review, handleEdit, onOpen, setIdToDelete, setIsOpenDel
                             setIdToDelete(review.id);
                             setIsOpenDelete(true);
                         }}>Eliminar</Button>
+                </Td>
+                <Td textAlign='center' display={{ base: 'block', md: 'none' }}>
+                    <Menu>
+                        <MenuButton>Opciones</MenuButton>
+                        <MenuList px='4' textColor='black' display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
+                            <Button bgColor='twitter.400'
+                                w='full'
+                                mb='4'
+                                textColor='white'
+                                _hover={{
+                                    background: "blue.400",
+                                    color: "none",
+                                }} onClick={() => {
+                                    handleEdit(review.id);
+                                    onOpen();
+                                }}>Editar</Button>
+                            <Button bgColor='red.600'
+                                w='full'
+                                textColor='white'
+                                _hover={{
+                                    background: "red.700",
+                                    color: "none",
+                                }} onClick={() => {
+                                    setIdToDelete(review.id);
+                                    setIsOpenDelete(true);
+                                }}>Eliminar</Button>
+                        </MenuList>
+                    </Menu>
                 </Td>
             </Tr>
         </>
